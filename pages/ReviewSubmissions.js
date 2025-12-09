6a5ea9850e3ada8cc129fff553b25a3998d3d6ce
GlennPena <gppena.student@ua.edu.ph>
2025-12-09 13:01:56 +0800

    Modified the API URLS for Render Deployment

--- pages/ReviewSubmissions.js
+++ pages/ReviewSubmissions.js
@@ -30,7 +30,7 @@ import { useFocusEffect } from '@react-navigation/native';
 
 
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = ' https://uact-backend.onrender.com/api';
 
 
 
